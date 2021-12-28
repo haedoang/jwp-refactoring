6c959dfddcfb54ccf834c5616b297101ad9d4ccc
haedoang <haedoang@gmail.com>
2021-12-29 01:54:14 +0900

    chore: 모듈 분리 작업 및 의존성 정리
    - Web, Api, Domain 기준으로 모듈 분리
    - 커스터마이징 예외처리가 다양한 계층에서 사용되어 수정 작업 필요
    - 공통 예외 케이스를 생성하여 각계층에서 구현해서 사용할 것 또는 공통 예외로 묶기

---
+++ kitchenpos-api/src/main/java/kitchenpos/table/TableService.java
@@ -0,0 +1,50 @@
+package kitchenpos.table;
+
+import kitchenpos.table.dto.ChangeEmptyRequest;
+import kitchenpos.table.dto.ChangeGuestNumberRequest;
+import kitchenpos.table.dto.OrderTableResponse;
+import kitchenpos.table.dto.OrderTableSaveRequest;
+import kitchenpos.table.exception.OrderTableNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class TableService {
+    private final OrderTableRepository orderTableRepository;
+    private final TableValidator tableValidator;
+
+    public TableService(OrderTableRepository orderTableRepository, TableValidator tableValidator) {
+        this.orderTableRepository = orderTableRepository;
+        this.tableValidator = tableValidator;
+    }
+
+    @Transactional
+    public OrderTableResponse create(final OrderTableSaveRequest request) {
+        OrderTable orderTable = orderTableRepository.save(request.toEntity());
+        return OrderTableResponse.of(orderTable);
+    }
+
+    public List<OrderTableResponse> list() {
+        return OrderTableResponse.ofList(orderTableRepository.findAll());
+    }
+
+    @Transactional
+    public OrderTableResponse changeEmpty(final Long orderTableId, final ChangeEmptyRequest request) {
+        OrderTable orderTable = orderTableRepository.findById(orderTableId)
+                .orElseThrow(OrderTableNotFoundException::new);
+        tableValidator.validateChangeEmpty(orderTable);
+        orderTable.changeEmpty(request.isEmpty());
+        return OrderTableResponse.of(orderTable);
+    }
+
+    @Transactional
+    public OrderTableResponse changeNumberOfGuests(final Long orderTableId, final ChangeGuestNumberRequest request) {
+        OrderTable orderTable = orderTableRepository.findById(orderTableId)
+                .orElseThrow(OrderTableNotFoundException::new);
+        orderTable.updateNumberOfGuests(request.getNumberOfGuests());
+        return OrderTableResponse.of(orderTable);
+    }
+}
